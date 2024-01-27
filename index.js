2e1bc6d5bb62f50ae3ebdc683b0d8a9204f9c2bd
Ao-chi <106396818+Ao-chi@users.noreply.github.com>
2024-01-27 13:31:34 +0800

    fix index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import bodyParser from "body-parser";
 import routes from "./routes/metaRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 const { metaRouter } = routes;
 
 app.use(bodyParser.json());
 app.use("/api.animeonline/", metaRouter);
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(port, () => console.log(`Server running on port: http://localhost:${port}`));
