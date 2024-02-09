0e9468b8e363c491f63aad0afa87c28252f9b183
Ao-chi <106396818+Ao-chi@users.noreply.github.com>
2024-02-09 16:43:01 +0800

    added basic cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import routes from "./routes/metaRoutes.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3001;
 const { metaRouter } = routes;
 
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 app.use(bodyParser.json());
 app.use("/api.animeonline/", metaRouter);
 
