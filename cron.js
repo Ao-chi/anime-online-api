6c7cab9c0881d8e885398a5a1d55185928bf718e
Ao-chi <106396818+Ao-chi@users.noreply.github.com>
2024-06-29 19:33:32 +0800

    "tried my fork"

--- cron.js
+++ cron.js
@@ -1,7 +1,9 @@
 import { CronJob } from "cron";
 import https from "https";
 
-const API_URL = "https://animeonline-api.onrender.com/api.animeonline/";
+// const API_URL = "https://animeonline-api.onrender.com/api.animeonline/";
+const API_URL = "https://anime-online-api-ocoi.onrender.com/api.animeonline/";
+
 let num = 1;
 const job = new CronJob(
     "*/14 * * * *",
